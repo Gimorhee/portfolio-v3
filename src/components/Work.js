d41fa766b7ee67fcbf2edd628d98fed3dc9fbfe5
Danny Rhee <dongyunrhee@gmail.com>
2021-04-22 13:53:21 -0700

    Edits on Work component

--- src/components/Work.js
+++ src/components/Work.js
@@ -4,6 +4,7 @@ import LS from "../images/projects/LS.png";
 import booster from "../images/projects/booster.png";
 import vsCorona from "../images/projects/vsCorona.png";
 import andreaCastro from "../images/projects/andreaCastro.png";
+import jess from "../images/projects/jess.png";
 
 export const Work = () => {
   return (
@@ -42,9 +43,9 @@ export const Work = () => {
                 <li>PHP</li>
               </ul>
               <div className="links">
-                <a href="">
+                {/* <a href="">
                   <i className="fab fa-github"></i>
-                </a>
+                </a> */}
                 <a href="https://learnditidaht.ca/" target="_blank" rel="noreferrer">
                   <i className="fas fa-external-link-alt"></i>
                 </a>
@@ -68,9 +69,9 @@ export const Work = () => {
                 <li>Realty API</li>
               </ul>
               <div className="links">
-                <a href="">
+                {/* <a href="">
                   <i className="fab fa-github"></i>
-                </a>
+                </a> */}
                 <a href="https://andreacastro.ca/home/" target="_blank" rel="noreferrer">
                   <i className="fas fa-external-link-alt"></i>
                 </a>
@@ -90,19 +91,45 @@ export const Work = () => {
             </a>
           </div>
 
-          {/* VSCORONA */}
+          {/* JESS RESTAURANT */}
           <div className="project1">
             <a
-              href="https://vscorona.netlify.app/"
+              href="https://jessrestaurant.ca/"
               target="_blank"
               rel="noreferrer"
               className="image"
               style={{
-                backgroundImage: `url(${vsCorona})`,
+                backgroundImage: `url(${jess})`,
               }}
             >
               <div className="imageOverlay"></div>
             </a>
+            <a className="detail" href="https://jessrestaurant.ca/" target="_blank" rel="noreferrer">
+              <h4>Featured Project</h4>
+              <h2>Jess' Restaurant</h2>
+              <p>
+                One of the Restaurant websites I created upon my client's design and functionality requirements. Co-operated with one of the desingers, and converted the design into the finalized web
+                product. From this website, customers are able to checkout the menus and make a reservation.
+              </p>
+              <ul>
+                <li>jQuery</li>
+                <li>Wordpress</li>
+                <li>PHP</li>
+                <li>SSL</li>
+              </ul>
+              <div className="links">
+                {/* <a href="https://github.com/Gimorhee/vsCorona" target="_blank" rel="noreferrer">
+                  <i className="fab fa-github"></i>
+                </a> */}
+                <a href="https://jessrestaurant.ca/" target="_blank" rel="noreferrer">
+                  <i className="fas fa-external-link-alt"></i>
+                </a>
+              </div>
+            </a>
+          </div>
+
+          {/* VSCORONA */}
+          <div className="project2">
             <a className="detail" href="https://vscorona.netlify.app/" target="_blank" rel="noreferrer">
               <h4>Featured Project</h4>
               <h2>vsCorona</h2>
@@ -117,7 +144,7 @@ export const Work = () => {
                 <li>Postman</li>
               </ul>
               <div className="links">
-                <a href="">
+                <a href="https://github.com/Gimorhee/vsCorona" target="_blank" rel="noreferrer">
                   <i className="fab fa-github"></i>
                 </a>
                 <a href="https://vscorona.netlify.app/" target="_blank" rel="noreferrer">
@@ -125,10 +152,32 @@ export const Work = () => {
                 </a>
               </div>
             </a>
+            <a
+              href="https://vscorona.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="image"
+              style={{
+                backgroundImage: `url(${vsCorona})`,
+              }}
+            >
+              <div className="imageOverlay"></div>
+            </a>
           </div>
 
           {/* BOOSTER */}
-          <div className="project2">
+          <div className="project1">
+            <a
+              href="https://boost3r.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+              className="image"
+              style={{
+                backgroundImage: `url(${booster})`,
+              }}
+            >
+              <div className="imageOverlay"></div>
+            </a>
             <a className="detail" href="https://boost3r.netlify.app/" target="_blank" rel="noreferrer">
               <h4>Featured Project</h4>
               <h2>Booster</h2>
@@ -143,25 +192,13 @@ export const Work = () => {
                 <li>Netlify</li>
               </ul>
               <div className="links">
-                <a href="">
+                <a href="https://github.com/Gimorhee/Booster" target="_blank" rel="noreferrer">
                   <i className="fab fa-github"></i>
                 </a>
                 <a href="https://boost3r.netlify.app/" target="_blank" rel="noreferrer">
                   <i className="fas fa-external-link-alt"></i>
                 </a>
               </div>
-            </a>
-
-            <a
-              href="https://boost3r.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-              className="image"
-              style={{
-                backgroundImage: `url(${booster})`,
-              }}
-            >
-              <div className="imageOverlay"></div>
             </a>
           </div>
         </div>
