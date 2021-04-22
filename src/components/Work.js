ef992be57b2ba8ebe880091ba5e5476b3c844e27
Danny Rhee <dongyunrhee@gmail.com>
2021-04-22 15:45:48 -0700

    OtherWorks finished

--- src/components/Work.js
+++ src/components/Work.js
@@ -1,10 +1,8 @@
 import React from "react";
 import "../styles/Work.scss";
-import LS from "../images/projects/LS.png";
-import booster from "../images/projects/booster.png";
-import vsCorona from "../images/projects/vsCorona.png";
-import andreaCastro from "../images/projects/andreaCastro.png";
-import jess from "../images/projects/jess.png";
+
+import { FeaturedWork } from "./Work/FeaturedWork";
+import { OtherWork } from "./Work/OtherWork";
 
 export const Work = () => {
   return (
@@ -14,193 +12,12 @@ export const Work = () => {
         <h1>Things I've Built</h1>
       </div>
 
-      <div className="projects">
-        <div className="projectContainer">
-          {/* LEARN DITIDAHT */}
-          <div className="project1">
-            <a
-              href="https://learnditidaht.ca/"
-              target="_blank"
-              rel="noreferrer"
-              className="image"
-              style={{
-                backgroundImage: `url(${LS})`,
-              }}
-            >
-              <div className="imageOverlay"></div>
-            </a>
-            <a className="detail" href="https://learnditidaht.ca/" target="_blank" rel="noreferrer">
-              <h4>Featured Project</h4>
-              <h2>Learn Ditidaht</h2>
-              <p>
-                Learn Ditidaht is an interactive language learning tool, built with an intention/effort to preserve the Ditidaht First Nation language as there are only 6 fluent speakers of their
-                native language on Vancouver Island. Try learning Ditidaht language!
-              </p>
-              <ul>
-                <li>React</li>
-                <li>Redux</li>
-                <li>Wordpress</li>
-                <li>PHP</li>
-              </ul>
-              <div className="links">
-                {/* <a href="">
-                  <i className="fab fa-github"></i>
-                </a> */}
-                <a href="https://learnditidaht.ca/" target="_blank" rel="noreferrer">
-                  <i className="fas fa-external-link-alt"></i>
-                </a>
-              </div>
-            </a>
-          </div>
-
-          {/* ANDREA CASTRO */}
-          <div className="project2">
-            <a className="detail" href="https://andreacastro.ca/home/" target="_blank" rel="noreferrer">
-              <h4>Featured Project</h4>
-              <h2>Andrea Castro Realty</h2>
-              <p>
-                Andrea Castro Realty website is one of the Real Estate websites I built upon my client's design and functionality requirements. The client and her clients are able to efficiently
-                communicate, access and retrieve the Real Estate information through the custom built IDX plugin implemented
-              </p>
-              <ul>
-                <li>jQuery</li>
-                <li>Wordpress</li>
-                <li>PHP</li>
-                <li>Realty API</li>
-              </ul>
-              <div className="links">
-                {/* <a href="">
-                  <i className="fab fa-github"></i>
-                </a> */}
-                <a href="https://andreacastro.ca/home/" target="_blank" rel="noreferrer">
-                  <i className="fas fa-external-link-alt"></i>
-                </a>
-              </div>
-            </a>
-
-            <a
-              href="https://andreacastro.ca/home/"
-              target="_blank"
-              rel="noreferrer"
-              className="image"
-              style={{
-                backgroundImage: `url(${andreaCastro})`,
-              }}
-            >
-              <div className="imageOverlay"></div>
-            </a>
-          </div>
-
-          {/* JESS RESTAURANT */}
-          <div className="project1">
-            <a
-              href="https://jessrestaurant.ca/"
-              target="_blank"
-              rel="noreferrer"
-              className="image"
-              style={{
-                backgroundImage: `url(${jess})`,
-              }}
-            >
-              <div className="imageOverlay"></div>
-            </a>
-            <a className="detail" href="https://jessrestaurant.ca/" target="_blank" rel="noreferrer">
-              <h4>Featured Project</h4>
-              <h2>Jess' Restaurant</h2>
-              <p>
-                One of the Restaurant websites I created upon my client's design and functionality requirements. Co-operated with one of the desingers, and converted the design into the finalized web
-                product. From this website, customers are able to checkout the menus and make a reservation.
-              </p>
-              <ul>
-                <li>jQuery</li>
-                <li>Wordpress</li>
-                <li>PHP</li>
-                <li>SSL</li>
-              </ul>
-              <div className="links">
-                {/* <a href="https://github.com/Gimorhee/vsCorona" target="_blank" rel="noreferrer">
-                  <i className="fab fa-github"></i>
-                </a> */}
-                <a href="https://jessrestaurant.ca/" target="_blank" rel="noreferrer">
-                  <i className="fas fa-external-link-alt"></i>
-                </a>
-              </div>
-            </a>
-          </div>
-
-          {/* VSCORONA */}
-          <div className="project2">
-            <a className="detail" href="https://vscorona.netlify.app/" target="_blank" rel="noreferrer">
-              <h4>Featured Project</h4>
-              <h2>vsCorona</h2>
-              <p>
-                Due to ongoing serious world-wide pandemic situation, I found myself googling to check up current status by going through various websites. I built vsCorona for myself, friends, and
-                families to easily and conveniently check up-to-date COVID-19 information in Canada, S. Korea, and the world.
-              </p>
-              <ul>
-                <li>Covid API</li>
-                <li>React</li>
-                <li>React-Chart</li>
-                <li>Postman</li>
-              </ul>
-              <div className="links">
-                <a href="https://github.com/Gimorhee/vsCorona" target="_blank" rel="noreferrer">
-                  <i className="fab fa-github"></i>
-                </a>
-                <a href="https://vscorona.netlify.app/" target="_blank" rel="noreferrer">
-                  <i className="fas fa-external-link-alt"></i>
-                </a>
-              </div>
-            </a>
-            <a
-              href="https://vscorona.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-              className="image"
-              style={{
-                backgroundImage: `url(${vsCorona})`,
-              }}
-            >
-              <div className="imageOverlay"></div>
-            </a>
-          </div>
-
-          {/* BOOSTER */}
-          <div className="project1">
-            <a
-              href="https://boost3r.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-              className="image"
-              style={{
-                backgroundImage: `url(${booster})`,
-              }}
-            >
-              <div className="imageOverlay"></div>
-            </a>
-            <a className="detail" href="https://boost3r.netlify.app/" target="_blank" rel="noreferrer">
-              <h4>Featured Project</h4>
-              <h2>Booster</h2>
-              <p>
-                Booster is a ping boosting service provider that I designed and developed for my fictional client. Booster provides users with a better internet connection, reduced latency, lower
-                ping, and far fewer lost packets to experience a superior gaming experience.
-              </p>
-              <ul>
-                <li>React</li>
-                <li>React-Spring</li>
-                <li>SCSS</li>
-                <li>Netlify</li>
-              </ul>
-              <div className="links">
-                <a href="https://github.com/Gimorhee/Booster" target="_blank" rel="noreferrer">
-                  <i className="fab fa-github"></i>
-                </a>
-                <a href="https://boost3r.netlify.app/" target="_blank" rel="noreferrer">
-                  <i className="fas fa-external-link-alt"></i>
-                </a>
-              </div>
-            </a>
-          </div>
+      <div className="projectsContainer">
+        <div className="featuredProjects">
+          <FeaturedWork />
+        </div>
+        <div className="otherProjects">
+          <OtherWork />
         </div>
       </div>
     </div>
---
+++ src/components/Work/OtherWork.js
@@ -0,0 +1,433 @@
+import React, { useState, Fragment } from "react";
+
+export const OtherWork = () => {
+  const [showMore, setShowMore] = useState(false);
+
+  return (
+    <div className="otherWorks">
+      <h2>Other Noteworthy Projects</h2>
+
+      {/* URBANFORM */}
+      <a className="otherWork" href="https://urbanform-frontend-staging.herokuapp.com/" target="_blank" rel="noreferrer">
+        <div className="topIcons">
+          <div className="left">
+            <i class="far fa-folder"></i>
+          </div>
+          <div className="right">
+            <a href="https://urbanform-frontend-staging.herokuapp.com/" target="_blank" rel="noreferrer">
+              <i className="fas fa-external-link-alt"></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="detail">
+          <h3>Urbanform</h3>
+          <p>Urbanform is the demo version of the website I created at Mapster for the client in Portland, which aggregates and calculates the spatial ramifications of building regulations. </p>
+        </div>
+
+        <div className="stacks">
+          <span>React</span>
+          <span>Redux</span>
+          <span>Firebase</span>
+          <span>GoogleMaps</span>
+        </div>
+      </a>
+
+      {/* PORTFOLIO2 */}
+      <a className="otherWork" href="https://gimorhee.github.io/portfolio-v2/" target="_blank" rel="noreferrer">
+        <div className="topIcons">
+          <div className="left">
+            <i class="far fa-folder"></i>
+          </div>
+          <div className="right">
+            <a href="https://github.com/Gimorhee/portfolio-v2" target="_blank" rel="noreferrer">
+              <i className="fab fa-github"></i>
+            </a>
+            <a href="https://gimorhee.github.io/portfolio-v2/" target="_blank" rel="noreferrer">
+              <i className="fas fa-external-link-alt"></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="detail">
+          <h3>Portfolio V2</h3>
+          <p>Previous version of my portfolio I developed with React</p>
+        </div>
+
+        <div className="stacks">
+          <span>React</span>
+          <span>ReactSpring</span>
+          <span>SCSS</span>
+          <span>Netlify</span>
+        </div>
+      </a>
+
+      {/* PALMETTO */}
+      <a className="otherWork" href="https://www.palmettopark.com/greenville" target="_blank" rel="noreferrer">
+        <div className="topIcons">
+          <div className="left">
+            <i class="far fa-folder"></i>
+          </div>
+          <div className="right">
+            <a href="https://github.com/mapstertech/palmetto-park" target="_blank" rel="noreferrer">
+              <i className="fab fa-github"></i>
+            </a>
+            <a href="https://www.palmettopark.com/greenville" target="_blank" rel="noreferrer">
+              <i className="fas fa-external-link-alt"></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="detail">
+          <h3>Palmetto Park Realty</h3>
+          <p>One of the Real Estate websites I created at Mapster for the client in US. Users are able to access the information they need more efficiently and conveniently </p>
+        </div>
+
+        <div className="stacks">
+          <span>React</span>
+          <span>Redux</span>
+          <span>Next</span>
+          <span>GoogleMaps</span>
+        </div>
+      </a>
+
+      {/* WATCH ME */}
+      <a className="otherWork" href="https://watchm3.netlify.app/" target="_blank" rel="noreferrer">
+        <div className="topIcons">
+          <div className="left">
+            <i class="far fa-folder"></i>
+          </div>
+          <div className="right">
+            <a href="https://github.com/Gimorhee/WatchMe" target="_blank" rel="noreferrer">
+              <i className="fab fa-github"></i>
+            </a>
+            <a href="https://watchm3.netlify.app/" target="_blank" rel="noreferrer">
+              <i className="fas fa-external-link-alt"></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="detail">
+          <h3>Watch Me</h3>
+          <p>
+            Watch Me is an application that specializes in watch trading, repairing and customizing. Watch Me offers a wide assortment of Classic and Luxury watch models and services to suffice all
+            customers needs{" "}
+          </p>
+        </div>
+
+        <div className="stacks">
+          <span>React</span>
+          <span>ReactSpring</span>
+          <span>SCSS</span>
+          <span>Netlify</span>
+        </div>
+      </a>
+
+      {/* NOMI MALK */}
+      <a className="otherWork" href="http://evaluations.asknomi.ca/" target="_blank" rel="noreferrer">
+        <div className="topIcons">
+          <div className="left">
+            <i class="far fa-folder"></i>
+          </div>
+          <div className="right">
+            <a href="http://evaluations.asknomi.ca/" target="_blank" rel="noreferrer">
+              <i className="fas fa-external-link-alt"></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="detail">
+          <h3>Nomi Malik Landing Page</h3>
+          <p>Custom designed landing page and evaluating website built for one of our clients, Nomi Malik, a Realtor, who specializes in marketing single family houses and condos in Vancouver </p>
+        </div>
+
+        <div className="stacks">
+          <span>Vue</span>
+          <span>PHPMailer</span>
+          <span>SCSS</span>
+          <span>GoogleMaps</span>
+        </div>
+      </a>
+
+      {/* PLATEAU */}
+      <a className="otherWork" href="https://plateau.herokuapp.com/" target="_blank" rel="noreferrer">
+        <div className="topIcons">
+          <div className="left">
+            <i class="far fa-folder"></i>
+          </div>
+          <div className="right">
+            <a href="https://github.com/Gimorhee/Plateau" target="_blank" rel="noreferrer">
+              <i className="fab fa-github"></i>
+            </a>
+            <a href="https://plateau.herokuapp.com/" target="_blank" rel="noreferrer">
+              <i className="fas fa-external-link-alt"></i>
+            </a>
+          </div>
+        </div>
+
+        <div className="detail">
+          <h3>Plateau</h3>
+          <p>
+            Plateau is my first Full-stack eCommerce website I built with MERN stacks. User login is required to complete orders and a confirmation email will be sent to user's provided email upon the
+            order completion
+          </p>
+        </div>
+
+        <div className="stacks">
+          <span>React</span>
+          <span>Redux</span>
+          <span>Node/Mongo</span>
+          <span>SendGrid</span>
+        </div>
+      </a>
+
+      {showMore === true && (
+        <Fragment>
+          {/* WESTPORT */}
+          <a className="otherWork" href="https://westportproperties.com/" target="_blank" rel="noreferrer">
+            <div className="topIcons">
+              <div className="left">
+                <i class="far fa-folder"></i>
+              </div>
+              <div className="right">
+                <a href="https://westportproperties.com/" target="_blank" rel="noreferrer">
+                  <i className="fas fa-external-link-alt"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="detail">
+              <h3>Westport Properties Inc.</h3>
+              <p>
+                One of the Real Estate websites built for the client. The client and his clients are able to efficiently communicate, access and retrieve the Real Estate information through the custom
+                built IDX plugin implemented
+              </p>
+            </div>
+
+            <div className="stacks">
+              <span>Wordpress</span>
+              <span>PHP</span>
+              <span>jQuery</span>
+              <span>API</span>
+            </div>
+          </a>
+
+          {/* RAIN MARKETING */}
+          <a className="otherWork" href="https://rainmarketing.ca/" target="_blank" rel="noreferrer">
+            <div className="topIcons">
+              <div className="left">
+                <i class="far fa-folder"></i>
+              </div>
+              <div className="right">
+                <a href="https://rainmarketing.ca/" target="_blank" rel="noreferrer">
+                  <i className="fas fa-external-link-alt"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="detail">
+              <h3>Rain Marketing</h3>
+              <p>Website I built for the client to assist them providing valuable sales and marketing strategies/services to their customers.</p>
+            </div>
+
+            <div className="stacks">
+              <span>Wordpress</span>
+              <span>PHP</span>
+              <span>jQuery</span>
+              <span>API</span>
+            </div>
+          </a>
+
+          {/* MIKE MALFORI */}
+          <a className="otherWork" href="https://www.mikemarfori.com/grandview-collection-at-grandview-heights/" target="_blank" rel="noreferrer">
+            <div className="topIcons">
+              <div className="left">
+                <i class="far fa-folder"></i>
+              </div>
+              <div className="right">
+                <a href="https://www.mikemarfori.com/grandview-collection-at-grandview-heights/" target="_blank" rel="noreferrer">
+                  <i className="fas fa-external-link-alt"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="detail">
+              <h3>Grandview Collection</h3>
+              <p>Advertising website I built for the client to advertise 'Grandview Collection' houses - currently sold out</p>
+            </div>
+
+            <div className="stacks">
+              <span>Wordpress</span>
+              <span>PHP</span>
+              <span>jQuery</span>
+              <span>API</span>
+            </div>
+          </a>
+
+          {/* BILL DRITSAS */}
+          <a className="otherWork" href="https://dritsasrealty.com/" target="_blank" rel="noreferrer">
+            <div className="topIcons">
+              <div className="left">
+                <i class="far fa-folder"></i>
+              </div>
+              <div className="right">
+                <a href="https://dritsasrealty.com/" target="_blank" rel="noreferrer">
+                  <i className="fas fa-external-link-alt"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="detail">
+              <h3>Bill Dritsas</h3>
+              <p>One of the Real Estate websites built for the client. Custom IDX plugin is implemented</p>
+            </div>
+
+            <div className="stacks">
+              <span>Wordpress</span>
+              <span>PHP</span>
+              <span>jQuery</span>
+              <span>API</span>
+            </div>
+          </a>
+
+          {/* FRASERPARK REALTY */}
+          <a className="otherWork" href="http://fraserparkrealty.com/" target="_blank" rel="noreferrer">
+            <div className="topIcons">
+              <div className="left">
+                <i class="far fa-folder"></i>
+              </div>
+              <div className="right">
+                <a href="http://fraserparkrealty.com/" target="_blank" rel="noreferrer">
+                  <i className="fas fa-external-link-alt"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="detail">
+              <h3>Fraserpark Realty</h3>
+              <p>Website built for advertising purposes. Fraserpark realty specializes in the Property Management of retail, office and industrial properties</p>
+            </div>
+
+            <div className="stacks">
+              <span>Wordpress</span>
+              <span>PHP</span>
+              <span>jQuery</span>
+              <span>API</span>
+            </div>
+          </a>
+
+          {/* DOLLAR TRACKER */}
+          <a className="otherWork" href="https://your-dollar-tracker.herokuapp.com/" target="_blank" rel="noreferrer">
+            <div className="topIcons">
+              <div className="left">
+                <i class="far fa-folder"></i>
+              </div>
+              <div className="right">
+                <a href="https://github.com/Gimorhee/DollarTracker" target="_blank" rel="noreferrer">
+                  <i className="fab fa-github"></i>
+                </a>
+                <a href="https://your-dollar-tracker.herokuapp.com/" target="_blank" rel="noreferrer">
+                  <i className="fas fa-external-link-alt"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="detail">
+              <h3>Dollar Tracker</h3>
+              <p>An expense calculating application built with React for the front and MongoDB for the backend. React Context was used to manage the application states</p>
+            </div>
+
+            <div className="stacks">
+              <span>React</span>
+              <span>ContextAPI</span>
+              <span>Node</span>
+              <span>MongoDB</span>
+            </div>
+          </a>
+
+          {/* LET's CHAT */}
+          <a className="otherWork" href="https://react-lets-chat.netlify.app/" target="_blank" rel="noreferrer">
+            <div className="topIcons">
+              <div className="left">
+                <i class="far fa-folder"></i>
+              </div>
+              <div className="right">
+                <a href="https://react-lets-chat.netlify.app/" target="_blank" rel="noreferrer">
+                  <i className="fas fa-external-link-alt"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="detail">
+              <h3>LetsChat</h3>
+              <p>A real-time chat application built with React, Node, Socket.io where users may create rooms and chat with others online</p>
+            </div>
+
+            <div className="stacks">
+              <span>React</span>
+              <span>SocketIO</span>
+              <span>Node</span>
+              <span>Heroku</span>
+            </div>
+          </a>
+
+          {/* PORTFOLIO V1 */}
+          <a className="otherWork" href="https://gimorhee.github.io/portfolio-v1/" target="_blank" rel="noreferrer">
+            <div className="topIcons">
+              <div className="left">
+                <i class="far fa-folder"></i>
+              </div>
+              <div className="right">
+                <a href="https://gimorhee.github.io/portfolio-v1/" target="_blank" rel="noreferrer">
+                  <i className="fas fa-external-link-alt"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="detail">
+              <h3>Portfolio V1</h3>
+              <p>Previous version of my portfolio built with jQuery</p>
+            </div>
+
+            <div className="stacks">
+              <span>jQuery</span>
+              <span>HTML</span>
+              <span>CSS</span>
+            </div>
+          </a>
+
+          {/* CARDS AGASINT THE INTERNET */}
+          <a className="otherWork" href="https://github.com/Gimorhee/Cards-Against-the-Internet" target="_blank" rel="noreferrer">
+            <div className="topIcons">
+              <div className="left">
+                <i class="far fa-folder"></i>
+              </div>
+              <div className="right">
+                <a href="https://github.com/Gimorhee/Cards-Against-the-Internet" target="_blank" rel="noreferrer">
+                  <i className="fab fa-github"></i>
+                </a>
+              </div>
+            </div>
+
+            <div className="detail">
+              <h3>Cards against the Internet</h3>
+              <p>A clone web app of a card game, Cards against Humanity built as a final project</p>
+            </div>
+
+            <div className="stacks">
+              <span>jQuery</span>
+              <span>HTML</span>
+              <span>CSS</span>
+            </div>
+          </a>
+        </Fragment>
+      )}
+
+      <div style={{ width: "100%" }}>
+        <button className="showMoreBtn" onClick={() => setShowMore(!showMore)}>
+          Show {showMore ? "Less" : "More"}
+        </button>
+      </div>
+    </div>
+  );
+};
